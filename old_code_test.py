31dcd7aa671b70d15de086cb7744078eaf71730c
hornedratt <kuzja.i01@mail.ru>
2024-05-12 01:39:31 +0700

    поняла, что коды с суффиксами article, current, test - не те на, на которых у меня были целевые графики

--- old_code_test.py
+++ old_code_test.py
@@ -10,6 +10,8 @@ import pandas as pd
 import random
 import numpy as np
 from sklearn.model_selection import train_test_split
+from src.visualization.figure_accuracy_per_epoch import losses_plot
+
 
 
 class customdataset(Dataset):
@@ -66,7 +68,7 @@ warnings.filterwarnings('ignore')
 # #делаем красивые индексы
 # MS_profiles.index = profiles_ID.index
 
-MS_profiles = pd.read_csv("D:\education\ML\Denoising_autoencoders_for_MALDI-TOF_MS-profiles_classification/data/processed/original_MS_profiles.csv", sep=';', header=None)
+MS_profiles = pd.read_csv("C:\education\ML\Denoising_autoencoders_for_MALDI-TOF_MS-profiles_classification/data/processed/original_MS_profiles.csv", sep=';', header=None)
 
 
 
@@ -104,7 +106,7 @@ MS_profiles_set = customdataset (MS_profiles.iloc[:, :12001].to_numpy(),
                                  MS_profiles.iloc[:, 12001].to_numpy(),
                                  MS_profiles.iloc[:, 12002].to_numpy())
 
-batch_s = 16
+batch_s = 1
 train, value = train_test_split(MS_profiles_set, train_size=0.7, shuffle=True)#получили value выбороку
 train_data = DataLoader(MS_profiles_set, batch_size=batch_s, shuffle=True)#а на train отправляем весь\
 #MS_profiles_set что бы модель знала профили всех имеющихся бактерий
@@ -115,7 +117,7 @@ autoencoder = vanilla_autoencoder().to(device)
 optimizer = torch.optim.Adam(autoencoder.parameters(), lr=0.001)
 
 from torch.cuda.memory import list_gpu_processes
-n_epochs = 50
+n_epochs = 100
 train_losses = []
 val_losses = []
 embaddings = torch.Tensor()
@@ -129,20 +131,62 @@ for epoch in range(n_epochs):
     train_losses_per_epoch = []
     for X_batch in train_data:
         noise = X_batch['profile'] + \
-        torch.FloatTensor(np.random.normal(loc=0.0, \
-                scale=noise_factor*X_batch['profile'], size= list(X_batch['profile'].size())))#шумим
+                torch.FloatTensor(np.random.normal(loc=0.0, \
+                                                   scale=noise_factor * X_batch['profile'],
+                                                   size=list(X_batch['profile'].size())))  # шумим
         noise = torch.abs(noise)
         y = torch.ones(list(X_batch['profile'].size()))
         noise = torch.where(noise < 1, noise, y)
-        X_batch['profile'] = X_batch['profile'].to(device)#чистые векторы
-        #print(X_batch['profile'].size().numpy())
+        X_batch['profile'] = X_batch['profile'].to(device)  # чистые векторы
+        # print(X_batch['profile'].size().numpy())
         noise = noise.to(device)
-        #print(noise.max())
+        # print(noise.max())
         optimizer.zero_grad()
-        reconstructed, embadding = autoencoder.forward(noise)#скармливаем шум
-        loss = L(reconstructed, X_batch['profile'])#сравниваем с читыми
+        reconstructed, embadding = autoencoder.forward(noise)  # скармливаем шум
+        loss = L(reconstructed, X_batch['profile'])  # сравниваем с читыми
         loss.backward()
         optimizer.step()
         train_losses_per_epoch.append(loss.item())
 
     train_losses.append(np.mean(train_losses_per_epoch))
+
+    autoencoder.eval()
+    val_losses_per_epoch = []
+    with torch.no_grad():
+        for i, X_batch in enumerate(value_data):
+            noise = X_batch['profile'] + \
+                    torch.FloatTensor(np.random.normal(loc=0.0, \
+                                                       scale=noise_factor * X_batch['profile'],
+                                                       size=list(X_batch['profile'].size())))  # шумим
+            noise = torch.abs(noise)
+            y = torch.ones(list(X_batch['profile'].size()))
+            noise = torch.where(noise < 1, noise, y)
+            noise = noise.to(device)
+            reconstructed, embadding = autoencoder(noise)
+            X_batch['profile'] = X_batch['profile'].to(device)
+            loss = L(reconstructed, X_batch['profile'])
+            val_losses_per_epoch.append(loss.item())
+            if (epoch >= (0)):
+                count = count + 1
+                embadding = embadding.to('cpu')
+                reconstructed = reconstructed.to('cpu')
+                X_batch['profile'] = X_batch['profile'].to('cpu')
+                if (i == 0) and (epoch == 0):
+                    embaddings = customdataset(embadding, X_batch['group'].copy(), X_batch['ID'].copy())
+                    truth = customdataset(X_batch['profile'], X_batch['group'].copy(), X_batch['ID'].copy())
+                    pred = customdataset(reconstructed, X_batch['group'].copy(), X_batch['ID'].copy())
+                    embadding = embadding.to(device)
+                if (i != 0) or (epoch != 0):
+                    embaddings.cat(embadding, X_batch['group'], X_batch['ID'])
+                    pred.cat(reconstructed, X_batch['group'], X_batch['ID'])
+                    truth.cat(X_batch['profile'], X_batch['group'], X_batch['ID'])
+                    embadding = embadding.to(device)
+
+    val_losses.append(np.mean(val_losses_per_epoch))
+
+# torch.save(autoencoder.encoder, output_path_model)
+train_losses = [float(arr) for arr in train_losses]
+val_losses = [float(arr) for arr in val_losses]
+losses_plot(train_losses=train_losses,
+            valid_losses=val_losses,
+            output_path='reports/figures/old_results_40%.png')
